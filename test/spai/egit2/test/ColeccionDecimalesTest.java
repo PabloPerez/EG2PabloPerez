39fdf9674e9472b421d3565e35f9bbb20afb4b9f
lienvaldesMIW <lien.valdesi@alumnos.upm.es>
2013-10-29 11:35:08 +0100

    Commit IssueTst#1 ColeccionDecimalesTest

--- test/spai/egit2/test/ColeccionDecimalesTest.java
+++ test/spai/egit2/test/ColeccionDecimalesTest.java
@@ -25,5 +25,9 @@ public class ColeccionDecimalesTest {
     public void testMayor() {
         assertEquals(3.4, this.numeros.mayor(), 1e-10);
     }
-
+    
+    @Test
+    public void testMultiplicacion() {
+        assertEquals(4.896, this.numeros.multiplicacion(), 1e-10);
+    }
 }
