eb65daed4fb84701152bacd6dc80902eed1b9b95
Sergio Esposito <Sergio Esposito@192.168.1.12>
2013-10-30 19:07:38 +0100

    IssueDev#4

--- src/spai/egit2/Fraccion.java
+++ src/spai/egit2/Fraccion.java
@@ -22,60 +22,62 @@ package spai.egit2;
  * denominador, y se suman o se restan los numeradores de las fracciones
  * equivalentes obtenidas
  * 
- * Multiplicaci�n: La multiplicaci�n de dos fracciones es otra fracci�n que
- * tiene: Por numerador el producto de los numeradores. Por denominador el
+ * Multiplicaci�n: La multiplicaci�n de dos fracciones es otra fracci�n
+ * que tiene: Por numerador el producto de los numeradores. Por denominador el
  * producto de los denominadores.
  * 
- * La divisi�n de dos fracciones es otra fracci�n que tiene: Por numerador el
- * producto de los extremos. Por denominador el producto de los medios. Invertir
- * fraccion
+ * La divisi�n de dos fracciones es otra fracci�n que tiene: Por numerador
+ * el producto de los extremos. Por denominador el producto de los medios.
+ * Invertir fraccion
  * 
  * @author jbernal
  * 
  */
 public class Fraccion {
-    private int numerador;
+	private int numerador;
 
-    private int denominador;
+	private int denominador;
 
-    public Fraccion(int numerador, int denominador) {
-        this.numerador = numerador;
-        this.denominador = denominador;
-    }
+	public Fraccion(int numerador, int denominador) {
+		this.numerador = numerador;
+		this.denominador = denominador;
+	}
 
-    public Fraccion() {
-        this(1, 1);
-    }
+	public Fraccion() {
+		this(1, 1);
+	}
 
-    public int getNumerador() {
-        return numerador;
-    }
+	public int getNumerador() {
+		return numerador;
+	}
 
-    public void setNumerador(int numerador) {
-        this.numerador = numerador;
-    }
+	public void setNumerador(int numerador) {
+		this.numerador = numerador;
+	}
 
-    public int getDenominador() {
-        return denominador;
-    }
+	public int getDenominador() {
+		return denominador;
+	}
 
-    public void setDenominador(int denominador) {
-        this.denominador = denominador;
-    }
+	public void setDenominador(int denominador) {
+		this.denominador = denominador;
+	}
 
-    public double decimal() {
-        return (double) numerador / denominador;
-    }
+	public double decimal() {
+		return (double) numerador / denominador;
+	}
 
-    public void invertir() {
-        
-    }
+	public void invertir() {
+		int numeradorAux = this.numerador;
+		this.setNumerador(this.denominador);
+		this.setDenominador(numeradorAux);
+	}
 
-    public boolean isImpropia() {
-        return numerador > denominador;
-    }
-    
-    public boolean isPropia() {
-        return numerador < denominador;
-    }
+	public boolean isImpropia() {
+		return numerador > denominador;
+	}
+
+	public boolean isPropia() {
+		return numerador < denominador;
+	}
 }
